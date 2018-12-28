8a9507c6cd3a85782b7515ea6bfed2c4e9556741
Lam Nhan <lamnhan92@gmail.com>
2018-12-28 12:16:48 +0700

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,13 @@
+import resolve from 'rollup-plugin-node-resolve';
+
 export default {
-    input: './dist/esm3/public_api.js',
-    output: [
-        {
-            file: './dist/fesm3/blank.js',
-            format: 'esm',
-            sourcemap: true
-        },
-        {
-            file: './dist/bundles/blank.umd.js',
-            format: 'umd',
-            sourcemap: true,
-            name: 'Blank'
-        }
+    input: './dist/index.js',
+    output: {
+        file: './dist/app.js',
+        format: 'umd',
+        name: 'App'
+    },
+    plugins: [
+        resolve()
     ]
 };
